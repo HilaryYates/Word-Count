b386df4e1ff5417ce8ccb818fc6f6ffa99dd7d10
hilaryyates <hilryyates@gmail.com>
2022-12-13 15:58:13 -0500

    first commit

---
+++ word-count/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import TextField from "./components/TextField";
+
+function App() {
+  return (
+    <div className='App'>
+      <TextField />
+    </div>
+  );
+}
+
+export default App;
---
+++ word-count/src/components/TextField.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import { useState } from "react";
+
+const TextField = () => {
+  // const { wordCount, setWordCount } = useState(0);
+  return (
+    <div>
+      <textarea rows='15' cols='80' id='TITLE'></textarea>
+    </div>
+  );
+};
+
+export default TextField;
